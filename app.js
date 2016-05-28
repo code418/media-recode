33f016c465216d8daa6e4b3c3392aea215c8545d
Richard Brown <richard@agilepixel.io>
2016-05-28 12:33:54 +0100

    library update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var sync = require('./routes/sync');
 
 var app = express();
 
@@ -20,11 +20,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/sync', sync);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/sync.js
+++ routes/sync.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET sync listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Syncing');
 });
 
 module.exports = router;
